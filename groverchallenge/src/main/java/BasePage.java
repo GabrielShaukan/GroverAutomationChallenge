f0bceb52f2aacb698039afe9776a7dfbdbc5f648
GabrielShaukan <gabrielshaukan@yahoo.com>
2021-10-03 18:49:36 +0200

    Added Base Page class

---
+++ groverchallenge/src/main/java/BasePage.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    WebDriver driver;
+    String url;
+
+    public BasePage(WebDriver driver, String url) {
+        this.driver = driver;
+        this.url = url;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //Method to open the browser and navigate to base url
+    public void openPage() {
+        driver.get(url);
+    }
+
+    //Method to open the browser and navigate to input url
+    public void openPage(String url) {
+        driver.get(url);
+    }
+
+    //Method to kill the driver
+    public void killDriver() {
+        driver.quit();
+    }
+}
